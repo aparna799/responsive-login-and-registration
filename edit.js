d3c10444646eda1365835233da44bb2b6c4e610c
aparna799 <110480454+aparna799@users.noreply.github.com>
2025-07-03 22:47:26 +0530

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const wrapper = document.querySelector('.wrapper');
+    const loginlink = document.querySelector('.login-link');
+    const registerlink = document.querySelector('.register-link');
+
+    registerlink.addEventListener('click', () => {
+        wrapper.classList.add('active');
+    });
+
+    loginlink.addEventListener('click', () => {
+        wrapper.classList.remove('active');
+    });
+});
